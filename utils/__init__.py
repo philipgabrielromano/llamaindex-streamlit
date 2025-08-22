44bd8bee4fcc9782bbe4699cab1972c965f055ff
philipgabrielromano <104011942+philipgabrielromano@users.noreply.github.com>
2025-08-22 09:35:22 -0400

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,32 @@
-# utils/__init__.py
+# utils/__init__.py (Complete version)
 from .document_processor import DocumentProcessor
 from .sharepoint_client import SharePointClient
 from .astra_client import AstraClient
-from .helpers import format_timestamp, calculate_time_diff, validate_file_type
+from .helpers import (
+    format_timestamp, 
+    calculate_time_diff, 
+    validate_file_type,
+    format_file_size,
+    create_processing_summary,
+    progress_tracker,
+    safe_json_display,
+    create_metrics_dataframe,
+    display_error_details,
+    sanitize_filename,
+    chunk_list,
+    get_cached_stats,
+    format_duration,
+    get_file_extension,
+    is_text_file,
+    is_office_file,
+    is_pdf_file,
+    create_file_summary,
+    filter_recent_items,
+    get_system_info,
+    create_status_indicator,
+    truncate_text,
+    calculate_statistics
+)
 
 __all__ = [
     'DocumentProcessor',
@@ -10,5 +34,25 @@ __all__ = [
     'AstraClient',
     'format_timestamp',
     'calculate_time_diff',
-    'validate_file_type'
+    'validate_file_type',
+    'format_file_size',
+    'create_processing_summary',
+    'progress_tracker',
+    'safe_json_display',
+    'create_metrics_dataframe',
+    'display_error_details',
+    'sanitize_filename',
+    'chunk_list',
+    'get_cached_stats',
+    'format_duration',
+    'get_file_extension',
+    'is_text_file',
+    'is_office_file',
+    'is_pdf_file',
+    'create_file_summary',
+    'filter_recent_items',
+    'get_system_info',
+    'create_status_indicator',
+    'truncate_text',
+    'calculate_statistics'
 ]
